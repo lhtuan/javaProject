7fbdc9710b6bf2b6c5603de043b9a75208884961
lhtuan <lh.tuan5392@gmail.com>
2013-12-16 14:24:09 +0700

    security

--- appliances/src/main/java/controller/HomeController.java
+++ appliances/src/main/java/controller/HomeController.java
@@ -8,7 +8,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	@RequestMapping(value={"/"},method = RequestMethod.GET)
-	public String welcome(ModelMap model)
+	public String defalut(ModelMap model)
+	{
+		return "home";
+	}
+	@RequestMapping(value={"/home"},method = RequestMethod.GET)
+	public String home(ModelMap model)
 	{
 		return "home";
 	}
